76599944cbabad9992525da46aa3d501a7802397
frostedonut2812 <sachinchopra1228@gmail.com>
2024-06-20 04:48:04 +0530

    Made Clock clock.js

--- src/game logic.jsx
+++ src/game logic.jsx
@@ -2,7 +2,8 @@ import {useState} from "react";
 
 function StartButton(){
     const[isrunning,setisrunning]=useState(true);
-    
+    const HandleClick = ()=>{setisrunning(!isrunning);}
+    return (<button onClick={HandleClick}>START GAME</button>)
 
 }
 
